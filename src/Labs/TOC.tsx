c53c965351eea8addbc54c5c23e683898a591e65
duncanmayer <duncmayer@gmail.com>
2025-02-10 17:10:43 -0500

    Everything before 3.7

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,25 +1,43 @@
 import Nav from "react-bootstrap/Nav";
+import { useLocation } from "react-router";
 // import { Link } from "react-router";
 export default function TOC() {
- return (
-   <Nav variant="pills">
-     <Nav.Item>
-       <Nav.Link href="#/Labs">Labs</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab1">Lab 1</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab2">Lab 2</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Labs/Lab3">Lab 3</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
-     </Nav.Item>
-     <Nav.Item>
-       <Nav.Link href="https://github.com/jannunzi">My GitHub</Nav.Link>
-     </Nav.Item>
-   </Nav>
-);}
+  const { pathname } = useLocation();
+  return (
+    <Nav variant="pills">
+      <Nav.Item>
+        <Nav.Link href="#/Labs">Labs</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          href="#/Labs/Lab1"
+          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}
+        >
+          Lab 1
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          href="#/Labs/Lab2"
+          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}
+        >
+          Lab 2
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link
+          href="#/Labs/Lab3"
+          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}
+        >
+          Lab 3
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="https://github.com/jannunzi">My GitHub</Nav.Link>
+      </Nav.Item>
+    </Nav>
+  );
+}
